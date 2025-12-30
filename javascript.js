96f8c1a426621608f684b783b09b8785bda02aa5
Jarin <jarinda086@gmail.com>
2025-12-30 16:33:06 +0300

    java

---
+++ javascript.js
@@ -0,0 +1,118 @@
+// How to Declare variables:
+let x = 5;
+let y = 6;
+
+// How to Compute values:
+let z = x + y;
+
+// I am a Comment. I do Nothing
+
+/* JavaScript Values include
+Literals(fixed values)
+Variables(Variable values)
+
+Javascript Literals:
+The most important syntax rules for literals (fixed values) are:
+
+Numbers are written with or without decimals:
+10.50
+1001
+
+Strings are text, written within double or single quotes:
+Example
+"John Doe"
+
+'John Doe'
+
+JavaScript Keywords
+JavaScript keywords are used to defines actions to be performed.
+
+The let and const keywords create variables:
+
+Example
+let x = 5;
+
+const fname = "John";
+Note:
+JavaScript keywords are case-sensitive.
+
+JavaScript does not interpret LET or Let as the keyword let.
+
+JavaScript Variables
+Variables are containers for storing data values.
+
+Variables must be identified with unique names.
+
+Example
+// Define x as a variable
+let x;
+
+// Assign the value 6 to x
+x = 6;
+
+JavaScript Identifiers
+An identifier is the name you give to a variable.
+
+Rules for identifiers:
+
+Must start with a letter, _, or $
+Can contain digits after the first character
+Cannot be a reserved keyword (let, const, if, etc.)
+Are case-sensitive
+JavaScript Operators
+JavaScript assignment operators (=) assign values to variables:
+
+Example
+let x = 5;
+let y = 6;
+let sum = x + y;
+JavaScript uses arithmetic operators ( + - * / ) to compute values:
+
+Example
+5 * 10
+JavaScript Expressions
+An expression is a combination of values, variables, and operators, which computes to a value.
+
+Examples
+(5 + 6) * 10 evaluates to 110:
+
+(5 + 6) * 10
+Expressions can also contain variable:
+
+x * 10
+"John" + " " + "Doe", evaluates to "John Doe":
+
+"John" + " " + "Doe"
+
+JavaScript is Case Sensitive
+JavaScript identifiers are case sensitive.
+
+The variables lastName and lastname, are different variables:
+
+Example
+let lastName = "Doe";
+let lastname = "Peterson";
+JavaScript and Camel Case
+Historically, programmers have used different ways of joining multiple words into one variable name:
+
+Hyphens:
+
+first-name, last-name, master-card, inter-city.
+
+Hyphens are not allowed in JavaScript. They are reserved for subtractions.
+
+Underscore:
+
+first_name, last_name, master_card, inter_city.
+
+Upper Camel Case (Pascal Case):
+
+FirstName, LastName, MasterCard, InterCity.
+
+
+Lower Camel Case:
+
+firstName, lastName, masterCard, interCity.
+
+JavaScript programmers tend to use lower camel case.
+*/
