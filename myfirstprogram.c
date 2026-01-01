962b63a778cc042463ff888795ef55d1db3fa276
Jarin <jarinda086@gmail.com>
2026-01-01 17:40:39 +0300

    almost done with lesson

--- myfirstprogram.c
+++ myfirstprogram.c
@@ -18,7 +18,7 @@ int main() {           // Body of the program starts here
   b = 20;  // Assigning value 20 to variable b
 
   //STEP 3: Variable Usage
-  result = a + b; // Adding values of a and b and storing it in result variable
+  result = a + b; // Adding values of a and b and storing it in result variable, a and b are operands, + is (binary)operator.
 
   printf( "The answer is = %d\n", result); // Printing the result on the screen, %d is a format specifier used for integer type.
   printf("The answer of %d + %d = %d\n", a, b, result);  // Printing values of a, b and result using format specifiers.
@@ -26,7 +26,7 @@ int main() {           // Body of the program starts here
   double c, d, result2;
   c = 15.76;
   d = 25.897;
-  result2 = c * d; // Multiplying values of c and d and storing it in result2 variable.
+  result2 = c * d; // Multiplying values of c and d and storing it in result2 variable. * is (binary) operator.
 
   printf("The answer is = %f\n", result2);
   printf("The answer of %f x %f = %f\n", c, d, result2); // Printing values of c, d and result2 using format specifiers. %f is used for float/double type.
