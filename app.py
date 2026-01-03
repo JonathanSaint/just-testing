e7eb299c35dca9fa9d86401feb313c800d088b7c
Saint <jarinda086@gmail.com>
2026-01-03 17:54:01 +0300

    Python for beginners

---
+++ app.py
@@ -0,0 +1,55 @@
+"""
+Mathematical Operators
+
++                    addition
+-                    subtraction
+/                    division
+**                   power
+//                   floor division
+%                    modulus
+*                    multiplication
+int(number)          integer
+float(number)        float
+"""
+
+print("Arinda Jonathan")
+#Py dog
+print("o----")
+print(" ||||")
+
+print("*" * 10) #Expression
+
+#Variables and data types
+price = 10
+price = 20 #integer
+rating = 4.5 #float
+name='Jonathan' #string
+is_published = False #boolean
+print(price)
+
+#Patient Check in
+name = "John Smith"
+age = 20
+is_new = True
+
+#Getting User input
+name = input("What is your name? ")
+print("Hi " + name)
+fav_color = input("What is your favorite color? ")
+print(name + " likes " + fav_color)
+
+birth_year = input("Birth year? ")
+print(type(birth_year))
+age = 2026 - int(birth_year)
+print(type(age))
+print(age)
+
+
+#weight converter
+#1 pound = 0.453592 Kg
+ask_weight = input("What is your weight(lbs)? ")
+weight = float(ask_weight) * float(0.453592)
+print("Your weight in Kg's is: " + str(weight))
+
+course = "Python Course for beginners"
+print(course)
