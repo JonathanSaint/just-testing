066166a0c05be0a46cc8cf9f4659dc38db6bbeb0
Saint <jarinda086@gmail.com>
2026-02-20 23:10:28 +0300

    did some real hard things bruhh

--- AITUTOR/lesson1.js
+++ AITUTOR/lesson1.js
@@ -186,3 +186,31 @@ function evenNumberCounter(arr) {
   return count;
 }
 console.log(evenNumberCounter(numbers));
+
+//Array Transformation
+
+const nums = [1, 2, 3, 4, 5];
+
+function doubleNumbers(arr) {
+  const result = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i] * 2);
+  }
+  return result
+}
+
+console.log(doubleNumbers([1, 2, 3, 4, 5]));
+
+
+function getEvenNumbers(arr) {
+let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+console.log(getEvenNumbers(nums));
